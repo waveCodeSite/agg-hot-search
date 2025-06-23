43312814b7ee797f3bf0f2ca8757667a0d7e17d0
dai yuanquan <131228850+waveCodeSite@users.noreply.github.com>
2025-06-23 14:56:24 +0800

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,8 @@ const ZHIHU_API_URL = 'https://60s-api.viki.moe/v2/zhihu';
 const TOUTIAO_API_URL = 'https://60s-api.viki.moe/v2/toutiao';
 const DOUYIN_API_URL = 'https://60s-api.viki.moe/v2/douyin';
 const TODAY_SUMMARY_API_URL = 'https://60s-api.viki.moe/v2/60s';
-const SHICI_DAILY_API_URL = 'https://www.meiriyiyan.com/api/v1';
+// const SHICI_DAILY_API_URL = 'https://www.meiriyiyan.com/api/v1';
+const HITOKOTO_API_URL = 'https://60s-api.viki.moe/v2/hitokoto';
 
 export async function fetchBiLiData() {
     const res = await fetch(BILI_API_URL, {cache: 'no-store'});
@@ -66,12 +67,22 @@ export async function fetchTodaySummaryData() {
     return json.data;
 }
 
-export async function fetchShiCiDailyData() {
-    const res = await fetch(SHICI_DAILY_API_URL, {cache: 'no-store'});
+export async function fetchHitokotoData() {
+    const res = await fetch(HITOKOTO_API_URL, {cache: 'no-store'});
     const json = await res.json();
     if (json.errors) {
         console.log(json.errors);
         throw new Error("Failed to fetch data");
     }
-    return json;
+    return json.data;
 }
+
+// export async function fetchShiCiDailyData() {
+//     const res = await fetch(SHICI_DAILY_API_URL, {cache: 'no-store'});
+//     const json = await res.json();
+//     if (json.errors) {
+//         console.log(json.errors);
+//         throw new Error("Failed to fetch data");
+//     }
+//     return json;
+// }
